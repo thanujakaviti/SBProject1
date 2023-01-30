2fc5ce09db8f49a6803fb3003a1077c36aa421cf
thanujakaviti <thanujakaviti@gmail.com>
2023-01-30 21:50:38 +0530

    project added

---
+++ SBProjec1/src/main/java/in/jt/SbProjec1Application.java
@@ -0,0 +1,13 @@
+package in.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbProjec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbProjec1Application.class, args);
+	}
+
+}
